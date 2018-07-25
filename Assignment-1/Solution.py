3a5fd96b7a82058e9dab9d285fb2dd0b6f60c892
ndvssankar <sivashankar@msitprogram.net>
2018-07-25 16:42:08 +0530

    Third Commit - used math.factorial

--- Assignment-1/Solution.py
+++ Assignment-1/Solution.py
@@ -1,27 +1,27 @@
-# Exercise: Assignment-1
-# Write a Python function, factorial(n), that takes in one number and returns the factorial of given number.
+''' Exercise: Assignment-1.'''
+# Write a Python function, factorial(n), that takes in one number and returns
+# the factorial of given number.
 
 # This function takes in one number and returns one number.
 
+import math
 
-def factorial(n):
+def factorial(value):
     '''
     n is positive Integer
 
     returns: a positive integer, the factorial of n.
     '''
     # Your code here
-    fact = 1
-    for i in range(1, n+1):
-        fact = fact * i
-    return fact
+    return math.factorial(value)
 
 
 def main():
-    data=input()
+    ''' Reading the data from the input and passing to the function factorial.'''
+    data = input()
     for i in range(int(data)):
         value = input()
         print(factorial(int(value)))
 
-if(__name__== "__main__"):
+if __name__ == "__main__":
     main()
