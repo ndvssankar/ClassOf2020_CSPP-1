bd1307ec7ccd8321cc8a876586a914525c906dcf
ndvssankar <sivashankar@msitprogram.net>
2018-07-25 16:27:07 +0530

    Second Version after passes all the test cases

--- Assignment-1/Solution.py
+++ Assignment-1/Solution.py
@@ -5,20 +5,23 @@
 
 
 def factorial(n):
-	'''
-	n is positive Integer
+    '''
+    n is positive Integer
 
-	returns: a positive integer, the factorial of n.
-	'''
-	# Your code here
-	
+    returns: a positive integer, the factorial of n.
+    '''
+    # Your code here
+    fact = 1
+    for i in range(1, n+1):
+        fact = fact * i
+    return fact
 
 
 def main():
-	data=input()
-	for i in range(int(data)):
-		a = input()
-		print(factorial(int(a)))	
+    data=input()
+    for i in range(int(data)):
+        value = input()
+        print(factorial(int(value)))
 
-if __name__== "__main__":
-	main()
+if(__name__== "__main__"):
+    main()
